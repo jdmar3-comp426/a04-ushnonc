4abce4c6812e040e98d4407da78a919a4b63e8b5
Ushno Chakraborty <ushnochakraborty@Ushnos-MBP.attlocal.net>
2021-11-28 22:10:53 -0500

    first couple requests

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(200).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (200)"})
+	res.status(200).json({"message": info.changes + " record updated: ID " + req.params.id + " (200)"})
 });
 
 
